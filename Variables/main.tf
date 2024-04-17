38d765bd928e2a9364fa38c5d2f5b9822e802196
aldan <adanilkumar@outlook.com>
2024-04-17 10:44:03 +0100

    added terraform config

--- Variables/main.tf
+++ Variables/main.tf
@@ -2,8 +2,7 @@ provider "aws" {
     region = "eu-west-2"
   
 }
-
 resource "aws_instance" "server1" {
-    ami = "ami-0c618421e207909d0"
+    ami = var.ami_id
     instance_type   = var.instance_type
     }
--- Variables/terraform.tfvars
+++ Variables/terraform.tfvars
@@ -1,1 +1,2 @@
+ami_id = "ami-0c618421e207909d0"
 instance_type = "t2.micro"
