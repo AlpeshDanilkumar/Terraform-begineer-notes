5f87433691ff5cb4cb94d4fae0ed19e1c587a3b0
aldan <adanilkumar@outlook.com>
2024-04-22 14:23:00 +0100

    added provisioner folder

---
+++ Provisioners/provisioner.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "first_ec2" {
+  ami           = "ami-0c618421e207909d0"
+  instance_type = "t2.micro"
+
+  provisioner "local-exec" {
+    command = "echo ${self.public_ip} >> server_ip.txt"
+    
+  }
+
+}
